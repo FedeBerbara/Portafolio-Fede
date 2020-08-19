3e6998abdf70e6d8532558b976a97ee4e74949d5
FedeBerbara <feedee96@gmail.com>
2020-08-19 08:29:23 -0300

    Subo arquitectura + 1ra version

---
+++ js/funciones.js
@@ -0,0 +1,15 @@
+// ScrollReveal
+
+ScrollReveal().reveal('.acerca-de');
+ScrollReveal().reveal('.tecnologias', {
+    delay: 500
+});
+ScrollReveal().reveal('.trabajos', {
+    delay: 500
+});
+ScrollReveal().reveal('.contacto', {
+    delay: 500
+});
+ScrollReveal().reveal('.redes-sociales', {
+    delay: 500
+});
